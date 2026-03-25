8cea22ebea79ca51eb3a19563c380586ff45fa84
Czmypc <czmypcs@gmail.com>
2026-03-25 21:58:30 +0530

    revert: remove github pages specific basePaths, readying for netlify deployment

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: isProd ? "/clothing-brand" : "",
-  assetPrefix: isProd ? "/clothing-brand/" : "",
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
